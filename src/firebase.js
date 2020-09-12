216341986f5b33a3dccd1afe1c9834057445e78f
Tomisin-O <onasanyatomisin@gmail.com>
2020-09-12 08:49:11 +0100

    Altered shopping basket

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZJBGYrPixV0vC1WEiYrWg53tIbrRUjYY",
+    authDomain: "challenge-1eb5b.firebaseapp.com",
+    databaseURL: "https://challenge-1eb5b.firebaseio.com",
+    projectId: "challenge-1eb5b",
+    storageBucket: "challenge-1eb5b.appspot.com",
+    messagingSenderId: "102374226986",
+    appId: "1:102374226986:web:49935cefbda0aff16c9753"
+  };
