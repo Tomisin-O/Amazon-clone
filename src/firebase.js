464d7ab0d6dfee232047ad68b47cabd2c6ffa9eb
Tomisin-O <onasanyatomisin@gmail.com>
2020-09-15 22:00:21 +0100

    User authentication with fire base

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from "firebase";
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyAZJBGYrPixV0vC1WEiYrWg53tIbrRUjYY",
     authDomain: "challenge-1eb5b.firebaseapp.com",
@@ -7,3 +10,11 @@ const firebaseConfig = {
     messagingSenderId: "102374226986",
     appId: "1:102374226986:web:49935cefbda0aff16c9753"
   };
+
+  
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
